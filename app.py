932a12b94c25f915ffb08139d0f71c9ccb044d2f
YaqoobIlyas <yaqoobilyas@gmail.com>
2024-10-08 20:18:30 +0500

    Second commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/submit', methods=['POST'])
 def submit_data():
